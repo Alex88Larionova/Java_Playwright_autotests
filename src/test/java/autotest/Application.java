ff943d58512e3a226ba4619cc06fa247fdd8d55e
Ларионова Александра Альбертовна <LarionovaA@MSK-MAC-9H9R3K.local>
2025-04-02 11:02:32 +0300

    Autotest files

---
+++ src/test/java/autotest/Application.java
@@ -0,0 +1,11 @@
+package test.java.autotest;
+
+import org.spingframework.boot.SpringApplication;
+import org.spingframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/test/java/autotest/RunCucumberTest.java
@@ -0,0 +1,8 @@
+package test.java.autotest;
+
+import io.cucumber.junit.platform.engine.Cucumber;
+
+@Cucumber // указывает на то, что это тестовый класс Cucumber
+public class RunCucumberTest {
+    
+} // не сожержит методов, потому что @Cucumber берет на себя задачу запуска тестов
